202081193d10c89cac96d5984cd066134275c55c
Sameer Chauhan <sameer55chauhan@gmail.com>
2023-12-30 02:09:39 +0530

    fix: get method to post as per the server

--- src/screens/BoardScreen.tsx
+++ src/screens/BoardScreen.tsx
@@ -19,7 +19,7 @@ import saveBoard from '../utils/saveBoard';
 
 export async function boardLoader() {
   try {
-    const data = await axios.get(`${import.meta.env.VITE_SERVER}/todo/board`, {
+    const data = await axios.post(`${import.meta.env.VITE_SERVER}/todo/board`, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('access_token')}`,
       },
